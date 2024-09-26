c36fc397fe6dbcfa077b0ae254c3eba9646c8960
LinaAlexandra2812 <linaalexdiaz@unicauca.edu.co>
2024-09-26 06:15:19 -0500

    Entidades necesarias para la creación de plugins

---
+++ OutlookPlugin/src/main/java/co/edu/unicacuca/mk/plugins/outlookplugin/OutlookPlugin.java
@@ -0,0 +1,19 @@
+package co.edu.unicacuca.mk.plugins.outlookplugin;
+
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+/**
+ *
+ * @author linit
+ */
+public class OutlookPlugin{
+    
+    
+}
---
+++ primer_proyecto_common/src/main/java/co/edu/unicauca/mk/common/interfaces/ISendEmail.java
@@ -0,0 +1,12 @@
+
+package co.edu.unicauca.mk.common.interfaces;
+
+/**
+ *
+ * @author linit
+ */
+public interface ISendEmail {
+    
+    void sendEmail(String username, String password, String addressee);
+    
+}
